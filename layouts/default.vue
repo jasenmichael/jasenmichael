8dd11ef0090f18d6e8d922ae03bb009e1e8393ff
jasenmichael <jasen@ready-builder.com>
2021-01-25 16:44:01 -0600

    chore: move padding to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="bg-gray-900 text-white">
-    <div class="flex flex-col">
+    <div class="flex flex-col min-h-screen">
       <Nav />
-      <Nuxt />
+      <Nuxt class="pt-32 md:pt-40 lg:pt-48 flex-grow" />
+      <Footer />
     </div>
   </div>
 </template>
