5c896616593397e21992a6a5406d42214494469a
jasenmichael <jasen@ready-builder.com>
2021-01-25 15:59:02 -0600

    chore: create landing page

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <HomeHead v-if="$route.path === '/'" class="flex-grow" />
+
+    <!-- <div class="bg-gray-100 text-gray-900">yo</div> -->
+    <!-- <Gallery :images="images" /> -->
+    <Gallery />
+    <!-- <pre>{{ images }}</pre> -->
+  </div>
+</template>
+
+<script>
+export default {
+  // async asyncData() {
+  //   const url =
+  //     'https://esits.org/tumblr-json.php?url=https://beautiful-redheads.tumblr.com'
+  //   const images = await fetch(url, { method: 'GET' })
+  //     .then((res) => res.json())
+  //     .then((res) =>
+  //       res.posts
+  //         .filter((post) => post.type === 'photo')
+  //         .map((photo) => {
+  //           return {
+  //             img: photo['photo-url-1280'],
+  //             imgThumb: photo['photo-url-1280'],
+  //             height: photo.height,
+  //             width: photo.width,
+  //             view: photo.height > photo.width ? 'portrait' : 'landscape',
+  //             // OG: photo,
+  //           }
+  //         })
+  //     )
+  //     .catch((error) => console.log('WTF!', error))
+  //   return { images }
+  // },
+}
+</script>
