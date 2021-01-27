05440e1e7240609c840804a77f68d99d3d439668
jasenmichael <jasen@ready-builder.com>
2021-01-27 01:44:02 -0600

    chore: create footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-green-400 text-gray-800">footer</div>
+  <div>
+    <div class="bg-red-900 text-yellow-50 h-36">footer</div>
+    <div class="bg-green-500 h-12"></div>
+  </div>
 </template>
 
 <script>
