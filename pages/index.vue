055a832d3861331fc00c897b66c1a7b9e8885ec4
jasenmichael <jasen@ready-builder.com>
2021-01-28 10:57:01 -0600

    chore: edit home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,6 @@
 <template>
-  <div>
-    <HomeHead v-if="$route.path === '/'" class="flex-grow" />
+  <div class="flex-grow">
+    <HomeHead />
+    <HomeAbout />
   </div>
 </template>
-
-<script>
-export default {
-  data: () => {
-    return {}
-  },
-  mounted() {
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  methods: {
-    handleScroll() {
-      this.scrollY = window.scrollY
-    },
-  },
-  // async asyncData() {
-  //   const url =
-  //     'https://esits.org/tumblr-json.php?url=https://beautiful-redheads.tumblr.com'
-  //   const images = await fetch(url, { method: 'GET' })
-  //     .then((res) => res.json())
-  //     .then((res) =>
-  //       res.posts
-  //         .filter((post) => post.type === 'photo')
-  //         .map((photo) => {
-  //           return {
-  //             img: photo['photo-url-1280'],
-  //             imgThumb: photo['photo-url-1280'],
-  //             height: photo.height,
-  //             width: photo.width,
-  //             view: photo.height > photo.width ? 'portrait' : 'landscape',
-  //             // OG: photo,
-  //           }
-  //         })
-  //     )
-  //     .catch((error) => console.log('WTF!', error))
-  //   return { images }
-  // },
-}
-</script>
