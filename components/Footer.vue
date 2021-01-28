aba48df3172dab098f899f646cf17a0efb07094e
jasenmichael <jasen@ready-builder.com>
2021-01-28 10:55:36 -0600

    chore: style footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="bg-red-900 text-yellow-50 h-36">footer</div>
-    <div class="bg-green-500 h-12"></div>
+    <div class="bg-green-500 text-yellow-50 h-36">footer</div>
+    <!-- <div class="bg-green-500 h-12"></div> -->
   </div>
 </template>
 
