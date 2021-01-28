ec0c8e43b5d7cda1510a0a449e3113197410b87f
jasenmichael <jasen@ready-builder.com>
2021-01-28 10:56:24 -0600

    chore: style default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-900 text-white">
     <div class="flex flex-col min-h-screen">
       <Nav />
-      <Nuxt class="pt-32 md:pt-40 lg:pt-48 flex-grow" />
+      <Nuxt class="pt-16 lg:pt-20 flex-grow" />
       <Footer />
     </div>
   </div>
