593478cf03dc7e744e79975fed7a8205fff7bfad
jasenmichael <jasen@ready-builder.com>
2021-02-12 16:06:11 -0600

    chore: edit stuff

--- components/Nav.vue
+++ components/Nav.vue
@@ -16,7 +16,6 @@
               class="text-green-400 bg-green-400 bg-clip-text fill-current h-10 z-50"
             /> -->
             <svg
-              v-show="isMounted"
               class="e7h61dww922a1 text-green-400 fill-current h-10 z-50"
               xmlns="http://www.w3.org/2000/svg"
               xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -209,6 +208,7 @@ export default {
   @apply items-center h-6 w-6 md:h-8 md:w-8 lg:h-12 lg:w-12 fill-current transform hover:rotate-6 hover:scale-110;
 }
 </style>
+
 <style>
 .e7h61dww922a2_to {
   animation: e7h61dww922a2_to__to 3000ms linear 1 normal forwards;
